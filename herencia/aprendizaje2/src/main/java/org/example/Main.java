17f175bf56b081b8c7f00110ee12f064c457c563
Matias Rossi <matias.rossi@despegar.com>
2023-08-30 19:47:06 -0300

    Correcciones menores

--- herencia/aprendizaje2/src/main/java/org/example/Main.java
+++ herencia/aprendizaje2/src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import org.example.modelos.Electrodomestico;
 import org.example.modelos.Lavadora;
 import org.example.modelos.Televisor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        // Ejercicio 2
         Lavadora lavadora = new Lavadora();
         lavadora.crearElectrodomestico();
         System.out.println("Precio lavadora = $" + lavadora.precioFinal());
@@ -12,5 +18,14 @@ public class Main {
         Televisor televisor = new Televisor();
         televisor.crearElectrodomestico();
         System.out.println("Precio lavadora = $" + lavadora.precioFinal());
+
+        /*
+        // Ejercicio 3
+        List<Electrodomestico> electrodomesticos = new ArrayList<>();
+
+        electrodomesticos.add(new Lavadora(1900.0, "azul", 'f', 0.0, 0.0));
+        electrodomesticos.add(new Televisor(1900.0, "azul", 'f', 0.0, 0.0, false));
+        System.out.println("Suma = $" + electrodomesticos.stream().mapToDouble(Electrodomestico::precioFinal).sum());
+        */
     }
 }
